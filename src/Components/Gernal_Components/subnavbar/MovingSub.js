d970304ce04d775726b4431a1609398836b6f569
codezone10 <codezone108@gmail.com>
2023-11-02 14:05:01 -0700

    Bug Resolve

--- src/Components/Gernal_Components/subnavbar/MovingSub.js
+++ src/Components/Gernal_Components/subnavbar/MovingSub.js
@@ -12,7 +12,7 @@ const MovingSub = ({setisHover}) => {
                     >
                         <div className='flex flex-col gap-y-10 pl-64 py-8 w-1/2'>
                         <Link className='text-2xl text-start flex items-end'>
-                            <img className="w-10 hover:shake " src="https://images.ctfassets.net/37vwfxlcawdb/5e2SSlRP2Oxtwh7V8TMlBU/61d589d79881b8faef21103b039c2263/Hemfrid_Piktogram_Flytthja__lp_64x64px-01.svg"  alt="" />
+                            <img className="w-10 hover:shake" src="https://images.ctfassets.net/37vwfxlcawdb/5e2SSlRP2Oxtwh7V8TMlBU/61d589d79881b8faef21103b039c2263/Hemfrid_Piktogram_Flytthja__lp_64x64px-01.svg"  alt="" />
                                <p className='px-4'>Everything For Your Move</p> 
                                <IoIosArrowForward className='ml-auto w-9 h-9'/>
                                </Link>
@@ -21,10 +21,13 @@ const MovingSub = ({setisHover}) => {
     <li className="square-border hover:font-semibold">Moving Service</li>
     <IoIosArrowForward className='ml-auto w-7 h-7' />
   </Link>
-  <Link to='/Window_clean' className='col-span-1 border-b-2 border-gr1 py-2 flex'>
-    <li className="square-border hover:font-semibold">Move Out Cleaning</li>
+  <Link to='/Moving_Home' className='col-span-1 border-b-2 border-gr1 py-2 flex'>
+  <li className="square-border">
+    <span>Move Out Cleaning</span>
     <IoIosArrowForward className='ml-auto w-7 h-7' />
-  </Link>
+  </li>
+</Link>
+
 </ul>
                             
 </div>
