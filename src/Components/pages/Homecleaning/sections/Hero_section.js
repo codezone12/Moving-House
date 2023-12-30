dffab90b3f08032b1490b25dc0413d813dd67565
codezone10 <codezone108@gmail.com>
2023-12-30 08:08:55 -0800

    Resolving

--- src/Components/pages/Homecleaning/sections/Hero_section.js
+++ src/Components/pages/Homecleaning/sections/Hero_section.js
@@ -112,29 +112,8 @@ const Hero = () => {
         </button>
         <div className="text-xl mt-10 flex items-start font-normal">
           <div className="mt-5">
-            {/* <div class="flex items-center">
-              <svg
-                height="24"
-                width="24"
-                viewBox="0 0 24 24"
-                color="white"
-                class="uiyb_O3 cfwk5OWE"
-                style={{ fill: "white" }}
-                className="hover:shake hover:border-yellow-400"
-              >
-                <g>
-                  <path d="m12 0a12 12 0 1 0 0 24 12 12 0 0 0 0-24zm0 22.286a10.286 10.286 0 1 1 0-20.572 10.286 10.286 0 0 1 0 20.572z"></path>
-                  <path d="m17.4 7.114-6.257 6.24-2.828-2.81a.848.848 0 1 0 -1.2 1.2l3.428 3.428a.824.824 0 0 0 1.2 0l6.857-6.858a.849.849 0 0 0 -1.2-1.2z"></path>
-                </g>
-              </svg>
-              <p class="ml-2">
-                <span className="animate-bounce text-gray-200 font-bold">
-                  Right Now!
-                </span>
-                <span className="text-orange-200 font-bold ml-2 mr-1">50%</span>
-              </p>
-              <p> discount on your first invoice</p>
-            </div> */}
+            {/* 
+             */}
             {/* <div className="my-6 text-base text-start">
               <p>
                 Use the code{" "}
